dc2b62554e673b2f8c710936b38a92ac864e913c
rifrifqyqy <rifqyh22@gmail.com>
2024-05-02 01:57:51 +0700

    menambah border papan iklan

--- src/components/papanIklan.jsx
+++ src/components/papanIklan.jsx
@@ -1,7 +1,7 @@
 export default function PapanIklan() {
   return (
     <>
-      <div className="w-full py-2 flex justify-center bg-gray-100">
+      <div className="w-full py-2 flex justify-center bg-gray-100 border-b-2 border-slate-200">
         <p>Program kesiapan karier, hadir untuk Anda. Daftar Sekarang!</p>
       </div>
     </>
